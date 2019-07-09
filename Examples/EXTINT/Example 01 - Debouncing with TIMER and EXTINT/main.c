59d8d8e51634b3e69edbe16c27f98545f00e51ce
JorgeHSantana <38670645+JorgeHSantana@users.noreply.github.com>
2019-07-09 15:09:02 -0300

    Update main.c

--- Examples/EXTINT/Example 01 - Debouncing with TIMER and EXTINT/main.c
+++ Examples/EXTINT/Example 01 - Debouncing with TIMER and EXTINT/main.c
@@ -41,7 +41,7 @@
   Toggle LED interruption.
 */
 void vToggleLEDInterrupt(void* vpArgs){
-  vDisableEXTINT(EXTINT_4);                                                     //Disabling external interruption on PINE4.
+  vDisableEXTINT(EXTINT_0);                                                     //Disabling external interruption on PINE4.
   vToggleGPIOPin(IO_GROUP_B, 7);                                                //Toggling LED.
   vResetTIMERCounter(TIMER_1);                                                  //Reseting TIMER_1 counter.
   vIgnoreTIMERRequest(TIMER_1, MASTER_TIMER);                                   //Ignoring all TIMER_1 interruptions.
@@ -52,8 +52,8 @@ void vToggleLEDInterrupt(void* vpArgs){
   Debouncing interruption.
 */
 void vDebouncingButton(void* vpArgs){
-  vIgnoreEXTINTRequest(EXTINT_4);                                               //Ignoring all external interrutions on PINE4 while EXTINT_4 is disabled.
-  vEnableEXTINT(EXTINT_4);                                                      //Enabling external interruption on PINE4.
+  vIgnoreEXTINTRequest(EXTINT_0);                                               //Ignoring all external interrutions on PINE4 while EXTINT_4 is disabled.
+  vEnableEXTINT(EXTINT_0);                                                      //Enabling external interruption on PINE4.
   vDisableTIMER(TIMER_1, MASTER_TIMER);                                         //Disabling TIMER_1 interrupt.
 }
 
@@ -71,9 +71,9 @@ int main(void){
   */
   vSetGPIOPinMode(IO_GROUP_E, 4, INPUT_MODE);                                   //Setting PINE4 to input mode (Button).
   vEnablePullUp(IO_GROUP_E, 4);                                                 //Enabling pullup resistor on PINE4.
-  vDefineEXTINTActivationMode(EXTINT_4, FALLING_BOARD_INTERRUPT);               //Setting interrupt activation mode on EXTINT_4 (PINE4... View on Pinout map of your ATMega).
-  vAttachEXTINTInterrupt(EXTINT_4, &vToggleLEDInterrupt, NULL);                 //Attaching external interruption function.
-  vEnableEXTINT(EXTINT_4);                                                      //Enabling external interruption.
+  vDefineEXTINTActivationMode(EXTINT_0, FALLING_BOARD_INTERRUPT);               //Setting interrupt activation mode on EXTINT_4 (PINE4... View on Pinout map of your ATMega).
+  vAttachEXTINTInterrupt(EXTINT_0, &vToggleLEDInterrupt, NULL);                 //Attaching external interruption function.
+  vEnableEXTINT(EXTINT_0);                                                      //Enabling external interruption.
 
   /*!
     Setting TIMER_1.
