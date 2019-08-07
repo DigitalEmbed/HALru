f6eddb504bfbcf234f9afd285a7c0a6d5f18ea2f
JorgeHSantana <38670645+JorgeHSantana@users.noreply.github.com>
2019-08-07 09:40:22 -0300

    Update main.c

--- Examples/USART/Example 01 - Echo/main.c
+++ Examples/USART/Example 01 - Echo/main.c
@@ -82,6 +82,8 @@ int main(void){
   vAttachUSARTInterrupt(USART_0, TX_COMPLETE, &vToggleLEDInterruption, NULL);
   vEnableTX(USART_0);
   vEnableRX(USART_0);
+  vEnableRXInterrupt(USART_0);
+  vEnableTXInterrupt(USART_0);
 
   /*!
     Enabling all interruptions.
